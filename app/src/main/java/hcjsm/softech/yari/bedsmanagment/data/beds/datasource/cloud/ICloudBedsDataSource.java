7b82cd972c17b01862d40f392612946acaee0b34
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-11-18 20:02:11 -0300

    se crea la interfaz icloudbeddatasource

---
+++ app/src/main/java/hcjsm/softech/yari/bedsmanagment/data/beds/datasource/cloud/ICloudBedsDataSource.java
@@ -0,0 +1,15 @@
+package hcjsm.softech.yari.bedsmanagment.data.beds.datasource.cloud;
+
+import java.util.List;
+
+import hcjsm.softech.yari.bedsmanagment.beds.domain.model.Bed;
+
+public interface ICloudBedsDataSource {
+    interface BedServiceCallback {
+        void onLoaded(List<Bed> beds);
+
+        void onError(String error);
+    }
+    void getBeds(BedServiceCallback callback);
+}
+
