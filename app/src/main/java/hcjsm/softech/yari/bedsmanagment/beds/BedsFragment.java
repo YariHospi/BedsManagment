f7d9c970d6dd903c43b5b83385e6fdb3fde76535
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-11-18 17:58:03 -0300

    se agregan los setup bed list, y refresh

--- app/src/main/java/hcjsm/softech/yari/bedsmanagment/beds/BedsFragment.java
+++ app/src/main/java/hcjsm/softech/yari/bedsmanagment/beds/BedsFragment.java
@@ -3,64 +3,83 @@ package hcjsm.softech.yari.bedsmanagment.beds;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import hcjsm.softech.yari.bedsmanagment.R;
+import hcjsm.softech.yari.bedsmanagment.beds.domain.model.Bed;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BedsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class BedsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView    mBedsList;
+    private BedsAdapter     mBedsAdapter;
 
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private View mEmptyView;
+    private BedsAdapter.BedItemListener mItemListener = new BedsAdapter.BedItemListener(){
+
+        @Override
+        public void onBedClick(Bed clickedNote) {
+
+        }
+    };
 
     public BedsFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BedsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BedsFragment newInstance(String param1, String param2) {
-        BedsFragment fragment = new BedsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static BedsFragment newInstance() {
+        return new BedsFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mBedsAdapter = new BedsAdapter(new ArrayList<Bed>(0),mItemListener);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beds, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_beds,container,false);
+
+        // UI references
+        mBedsList = (RecyclerView) root.findViewById(R.id.beds_list);
+        mEmptyView = root.findViewById(R.id.noBeds);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.refresh_layout);
+
+        //Setup
+        setUpBedsList();
+        setUpRefreshLayout();
+
+        return root;
+    }
+
+    private void setUpRefreshLayout() {
+        mSwipeRefreshLayout.setColorSchemeColors(
+                ContextCompat.getColor(getActivity(), R.color.colorPrimary),
+                ContextCompat.getColor(getActivity(), R.color.colorAccent),
+                ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+        });
+    }
+
+    private void setUpBedsList() {
+        mBedsList.setAdapter(mBedsAdapter);
+        mBedsList.setHasFixedSize(true);
     }
 
 }
