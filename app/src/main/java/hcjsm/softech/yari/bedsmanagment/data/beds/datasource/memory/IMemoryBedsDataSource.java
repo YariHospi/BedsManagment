38d5aca53b92ec6a954a1cec45ea01ffbe667165
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-11-18 21:45:30 -0300

    se modifica el metodo find

--- app/src/main/java/hcjsm/softech/yari/bedsmanagment/data/beds/datasource/memory/IMemoryBedsDataSource.java
+++ app/src/main/java/hcjsm/softech/yari/bedsmanagment/data/beds/datasource/memory/IMemoryBedsDataSource.java
@@ -2,10 +2,11 @@ package hcjsm.softech.yari.bedsmanagment.data.beds.datasource.memory;
 
 import java.util.List;
 
+import hcjsm.softech.yari.bedsmanagment.beds.domain.criteria.IBedCriteria;
 import hcjsm.softech.yari.bedsmanagment.beds.domain.model.Bed;
 
 public interface IMemoryBedsDataSource {
-    List<Bed> find(BedCriteria criteria);
+    List<Bed> find(IBedCriteria criteria);
 
     void save(Bed bed);
 
